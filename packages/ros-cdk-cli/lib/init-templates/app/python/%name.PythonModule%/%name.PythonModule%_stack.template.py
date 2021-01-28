3c92a2d1b213199ecd7a4f1a069f9ffcd28f25a6
Ehzyo <fytyllovewhy@gmail.com>
2021-01-28 15:16:09 +0800

    delete all packages

--- packages/ros-cdk-cli/lib/init-templates/app/python/%name.PythonModule%/%name.PythonModule%_stack.template.py
+++
@@ -1,9 +0,0 @@
-from ros_cdk_core import *
-
-
-class %name.PascalCased%Stack(Stack):
-
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
-
-        # The code that defines your stack goes here
